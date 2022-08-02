201893e658ed8bbdc9291a07e72e0a581bc1a9ef
Tabalin Nikolay <Zerru_Kun@yahoo.com>
2022-08-02 14:53:02 +0300

    Подготовка.

---
+++ src/Counter.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+const Counter = () => {
+  return (
+    <div className="counter">
+      <div className="btns">
+        <button className="btn">+</button>
+        <button className="btn">-</button>
+      </div>      
+    </div>
+  );
+};
+
+export default Counter;
